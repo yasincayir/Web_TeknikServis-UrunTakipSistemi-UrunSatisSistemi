7ffb858f4688c0859ba872fbc42a9d1b1acd0f0a
yasincayir <cayir.yasinn@gmail.com>
2021-05-10 19:37:32 +0300

    Add project files.

---
+++ Teknik_Servis_Web/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Teknik_Servis_Web.Entity;
+namespace Teknik_Servis_Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TblHakkımızda.ToList();
+            Repeater1.DataBind();
+
+            Repeater2.DataSource = db.TblUrun.ToList();
+            Repeater2.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Tblİletisim t = new Tblİletisim();
+            t.ADSOYAD = TextBox1.Text;
+            t.MAİL = TextBox2.Text;
+            t.KONU = TextBox3.Text;
+            t.MESAJ = TextBox4.Text;
+            db.Tblİletisim.Add(t);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Teknik_Servis_Web/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Teknik_Servis_Web.Entity;
+namespace Teknik_Servis_Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //var degerler = db.TblUrunTakip.ToList();
+            //Repeater1.DataSource = degerler;
+            //Repeater1.DataBind();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var degerler = db.TblUrunTakip.Where(x => x.SERİNO == TextBox1.Text);
+            Repeater1.DataSource = degerler.ToList();
+            Repeater1.DataBind();
+        }
+    }
+}
